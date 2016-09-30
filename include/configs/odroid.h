ff4519edf25092f9aa79422391d0720706bf545c
Zoltan Tombol <zoltan.tombol@gmail.com>
2016-10-01 00:35:11 +0200

    odroid: Refactor and comment DFU settings

--- include/configs/odroid.h
+++ include/configs/odroid.h
@@ -96,35 +96,6 @@
 
 #define CONFIG_DEFAULT_CONSOLE		"console=ttySAC1,115200n8\0"
 
-/* Partitions name */
-#define PARTS_BOOT		"boot"
-#define PARTS_ROOT		"platform"
-
-#define CONFIG_DFU_ALT \
-	"uImage fat 0 1;" \
-	"zImage fat 0 1;" \
-	"Image.itb fat 0 1;" \
-	"uInitrd fat 0 1;" \
-	"exynos4412-odroidu3.dtb fat 0 1;" \
-	"exynos4412-odroidx2.dtb fat 0 1;" \
-	""PARTS_BOOT" part 0 1;" \
-	""PARTS_ROOT" part 0 2\0" \
-
-#define CONFIG_SET_DFU_ALT_INFO
-#define CONFIG_SET_DFU_ALT_BUF_LEN	SZ_1K
-
-#define CONFIG_DFU_ALT_BOOT_EMMC \
-	"u-boot raw 0x3e 0x800 mmcpart 1;" \
-	"bl1 raw 0x0 0x1e mmcpart 1;" \
-	"bl2 raw 0x1e 0x1d mmcpart 1;" \
-	"tzsw raw 0x83e 0x138 mmcpart 1\0"
-
-#define CONFIG_DFU_ALT_BOOT_SD \
-	"u-boot raw 0x3f 0x800;" \
-	"bl1 raw 0x1 0x1e;" \
-	"bl2 raw 0x1f 0x1d;" \
-	"tzsw raw 0x83f 0x138\0"
-
 
 /*----------------------------------------------------------------------
  * Physical Memory Map
@@ -166,6 +137,47 @@
 
 /* Start address of boot monitor code. */
 #define CONFIG_SYS_MONITOR_BASE	0x00000000
+
+
+/*----------------------------------------------------------------------
+ * Device Firmware Upgrade
+ *--------------------------------------------------------------------*/
+
+/*
+ * Set `dfu_alt_info' at runtime by concatenating `dfu_alt_boot' and
+ * `dfu_alt_system'. See `doc/README.odroid' for more.
+ */
+#define CONFIG_SET_DFU_ALT_INFO
+#define CONFIG_SET_DFU_ALT_BUF_LEN	SZ_1K
+
+/* Partition names */
+#define PARTS_BOOT	"boot"
+#define PARTS_ROOT	"root"
+
+/* Value of `dfu_alt_system' set by `CONFIG_EXTRA_ENV_SETTINGS'. */
+#define CONFIG_DFU_ALT_SYSTEM \
+	"uImage fat 0 1; "                   \
+	"zImage fat 0 1; "                   \
+	"Image.itb fat 0 1; "                \
+	"uInitrd fat 0 1; "                  \
+	"exynos4412-odroidu3.dtb fat 0 1; "  \
+	"exynos4412-odroidx2.dtb fat 0 1; "  \
+	""PARTS_BOOT" part 0 1; "            \
+	""PARTS_ROOT" part 0 2; \0"
+
+/* Value of `dfu_alt_boot' when booting from an eMMC module. */
+#define CONFIG_DFU_ALT_BOOT_EMMC \
+	"u-boot raw 0x3e 0x800 mmcpart 1; "  \
+	"bl1 raw 0x0 0x1e mmcpart 1; "       \
+	"bl2 raw 0x1e 0x1d mmcpart 1; "      \
+	"tzsw raw 0x83e 0x138 mmcpart 1; \0"
+
+/* Value of `dfu_alt_boot' when booting from an SD card. */
+#define CONFIG_DFU_ALT_BOOT_SD \
+	"u-boot raw 0x3f 0x800; "            \
+	"bl1 raw 0x1 0x1e; "                 \
+	"bl2 raw 0x1f 0x1d; "                \
+	"tzsw raw 0x83f 0x138; \0"
 
 
 /*----------------------------------------------------------------------
@@ -223,7 +235,7 @@
 
 /* Device Firmware Upgrade. */
 #define BOOTENV_DFU \
-	"dfu_alt_system=" CONFIG_DFU_ALT \
+	"dfu_alt_system=" CONFIG_DFU_ALT_SYSTEM \
 	"dfu_alt_info=Please reset the board\0"
 
 /* Console settings and related command variables. */
